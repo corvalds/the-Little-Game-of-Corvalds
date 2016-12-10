5d70b1864ef4a2c8a42b18886bff9ceb03243f2a
GTX560 <singmig@163.com>
2016-12-10 18:31:03 +0800

    compele the base func

--- 2048 in C/2048.c
+++ 2048 in C/2048.c
@@ -7,23 +7,27 @@
 #define MID_POS 3
 #define MAX_DIGIT 8
 #define EMPTY_REACT "       "
+#define FINALGOAL 2048
 
+int gameOver = 0;
+int invalidAction = 0;
 typedef struct{
 	int x, y;
+	int isAdd;
 	char value[MAX_DIGIT];
 }React;
 typedef struct{
-	React reacts[ROWNUM * COLNUM];
+	React reacts[ROWNUM][COLNUM];
 	int emptyReact; //场地中空的方块的个数 
 }PlayGround;
 
 void InitPlayGround(PlayGround *ground){
 	int x, y, pos;
 	
-	for(x = 1;x <= ROWNUM;x++){
-		for(y = 1;y <= COLNUM;y++){
-			pos = (x - 1) * COLNUM + y - 1;
-			strcpy(ground->reacts[pos].value, EMPTY_REACT);
+	for(x = 0;x < ROWNUM;x++){
+		for(y = 0;y < COLNUM;y++){
+			strcpy(ground->reacts[x][y].value, EMPTY_REACT);
+			ground->reacts[x][y].isAdd = 0;
 		}
 	}
 	ground->emptyReact = ROWNUM * COLNUM;
@@ -31,49 +35,192 @@ void InitPlayGround(PlayGround *ground){
 
 //优化随机生成的算法 
 void CreateNewElem(PlayGround *ground){
-	int x, y, pos;
-	int i; //定义循环变量 
+	int x, y;
+	int i;
 	
 	srand((unsigned)time(NULL));
-	x = rand() % 5;
-	y = rand() % 5;
-	pos = (x - 1) * COLNUM + y -1;
+	x = rand() % (ROWNUM + 1);
+	y = rand() % (COLNUM + 1);
 	if(ground->emptyReact != 0){
-		while(strcmp(ground->reacts[pos].value, EMPTY_REACT) != 0){	
-			x = rand() % 5;
-			y = rand() % 5;
-			pos = (x - 1) * COLNUM + y -1;
+		while(strcmp(ground->reacts[x][y].value, EMPTY_REACT) != 0){	
+			x = rand() % (ROWNUM + 1);
+			y = rand() % (COLNUM + 1);
 		}
-		ground->reacts[pos].value[MID_POS] = '2';
+		ground->reacts[x][y].value[MID_POS] = '2';
 		ground->emptyReact--;
 	}
 		
 }
 
 void PrintPlayGround(PlayGround *ground){
-	int x, y = 1, pos;
+	int x, y;
 	
 	printf("\t################################################\n");
-	for(x = 1;x <= ROWNUM;x++){
-		pos = (x - 1) * COLNUM + y - 1;
+	for(x = 0;x < ROWNUM;x++){
 		printf("\t#  * * * * *  * * * * *  * * * * *  * * * * *  #\n");
 		printf("\t#  *       *  *       *  *       *  *       *  #\n");
-		printf("\t#  *%s*  *%s*  *%s*  *%s*  #\n", ground->reacts[pos].value, ground->reacts[pos + 1].value, ground->reacts[pos + 2].value, ground->reacts[pos + 3].value);
+		printf("\t#");
+		for(y = 0;y < COLNUM;y++)
+			printf("  *%s*", ground->reacts[x][y].value);
+		printf("  #\n");
 		printf("\t#  *       *  *       *  *       *  *       *  #\n");
 		printf("\t#  * * * * *  * * * * *  * * * * *  * * * * *  #\n");
-		if(x != ROWNUM)
+		if(x != ROWNUM - 1)
 			printf("\t#                                              #\n");
 	}
 	printf("\t################################################\n");
 }
 
+void ReactAddition(PlayGround *ground, int hor, int ver, int preHor, int preVer){
+	int tmpInt, iterInt, tmpStrLen, forward;
+	char tmpStr[MAX_DIGIT];
+	
+	if(strcmp(ground->reacts[hor][ver].value, EMPTY_REACT) == 0)
+		return;
+	if(strcmp(ground->reacts[preHor][preVer].value, EMPTY_REACT) == 0){
+		strcpy(ground->reacts[preHor][preVer].value, ground->reacts[hor][ver].value);
+		strcpy(ground->reacts[hor][ver].value, EMPTY_REACT);
+		invalidAction = 0;
+	} else if(strcmp(ground->reacts[preHor][preVer].value, ground->reacts[hor][ver].value) == 0 && !ground->reacts[preHor][preVer].isAdd && !ground->reacts[hor][ver].isAdd){
+		tmpInt = atoi(ground->reacts[preHor][preVer].value) + atoi(ground->reacts[hor][ver].value);
+		if(tmpInt == FINALGOAL)
+			gameOver = 1;
+		itoa(tmpInt, tmpStr, 10);
+		tmpStrLen = strlen(tmpStr);
+		forward = tmpStrLen - 2;
+		for(iterInt = 0;iterInt < tmpStrLen;iterInt++){
+			if(tmpStrLen < 3)
+				ground->reacts[preHor][preVer].value[MID_POS + iterInt] = tmpStr[iterInt];
+			else
+				ground->reacts[preHor][preVer].value[MID_POS + iterInt - forward] = tmpStr[iterInt];
+		}
+		strcpy(ground->reacts[hor][ver].value, EMPTY_REACT);
+		ground->reacts[preHor][preVer].isAdd = 1;
+		ground->emptyReact++;
+		invalidAction = 0;
+	}
+}
+
+void ReactGlide(PlayGround *ground, int isHorizontal, int moveDirection, int x, int y){
+	int hor = x, ver = y, preHor = x, preVer = y;
+	
+	while(1){
+		if(isHorizontal){
+			ver = preVer;
+			if(moveDirection){
+				preVer--;
+				if(preVer < 0)
+					break;
+			} else{
+				preVer++;
+				if(preVer >= COLNUM)
+					break;
+			}
+		} else{
+			hor = preHor;
+			if(moveDirection){
+				preHor--;
+				if(preHor < 0)
+					break;
+			} else{
+				preHor++;
+				if(preHor >= ROWNUM)
+					break;
+			}
+		}
+		ReactAddition(ground, hor, ver, preHor, preVer);
+	}
+}
+
+void Move(PlayGround *ground, int isHorizontal, int moveDirection){
+	int iterEnd = 0, isFirst = 1;
+	int x, y, hor, ver, preHor, preVer;
+	
+	//对x，y进行初始化
+	if(moveDirection){
+		//a,w 
+		x = -1; 
+		y = -1;
+	} else{
+		//d,s
+		x = isHorizontal?ROWNUM:COLNUM;
+		y = isHorizontal?COLNUM:ROWNUM;
+	}
+	//选择方块进行移动 
+	while(1){
+		if(moveDirection){
+			x++;
+			y = -1;
+		}
+		else{
+			x--;
+			y = isHorizontal?COLNUM:ROWNUM;
+		}
+		isFirst = 1;
+		while(1){
+			if(moveDirection){
+				y++;
+				hor = isHorizontal?x:y;
+				ver = isHorizontal?y:x;
+			} else{
+				y--;
+				hor = isHorizontal?x:y;
+				ver = isHorizontal?y:x;
+			} 
+			//这里还可以优化 
+			if(isHorizontal && (hor < 0 || hor >= ROWNUM)){
+				iterEnd = 1;
+				break;
+			} else if(!isHorizontal && (ver < 0 || ver >= COLNUM)){
+				iterEnd = 1;
+				break;
+			}
+			if(isHorizontal && (ver < 0 || ver >= COLNUM))
+				break;
+			else if(!isHorizontal && (hor < 0 || hor >= ROWNUM))
+				break;
+			//方块移动 
+			ReactGlide(ground, isHorizontal, moveDirection, hor, ver);
+		}
+		if(iterEnd)
+			break;
+	}
+	for(x = 0;x < ROWNUM;x++)
+		for(y = 0;y < COLNUM;y++)
+			ground->reacts[x][y].isAdd = 0;
+}
+
+void HandleOrder(PlayGround *ground, char order){
+	switch(order){
+		case 65:;
+		case 97:Move(ground, 1, 1);break; //a
+		case 68:;
+		case 100:Move(ground, 1, 0);break; //d
+		case 83:;
+		case 115:Move(ground, 0, 0);break; //s
+		case 87:;
+		case 119:Move(ground, 0, 1);break; //w
+	}
+}
+
 int main(){
 	PlayGround *ground;
+	char order;
 	
 	ground = malloc(sizeof(PlayGround));
 	InitPlayGround(ground);
-	CreateNewElem(ground);
-	PrintPlayGround(ground);
+	while(1){
+		if(!invalidAction)
+			CreateNewElem(ground);
+		invalidAction = 1;
+		PrintPlayGround(ground);
+		order = getch();
+		fflush(stdin);
+		HandleOrder(ground, order);
+		if(gameOver)
+			break;
+		system("cls");
+	}
 	
 	return 0;
 }
